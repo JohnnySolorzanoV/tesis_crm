fa488123d84a10f5be71849d7e9148f7770325f0
Jonnathan Solorzano Vera <johnnyjsolver99@gmail.com>
2026-04-21 23:52:48 -0500

    corregir nombre de variable para la contraseña en la función CrearCuenta

--- backend/use_cases/crearCuenta.js
+++ backend/use_cases/crearCuenta.js
@@ -4,8 +4,8 @@ const { UsuarioRepository } = require('../repositories/UsuarioRepository');
 
 
 async function CrearCuenta({ cedula, nombre, correo, contrasena, rol }) {
-  const hashedPassword = await bcrypt.hash(contrasena, 10);
-  const usuario = new Usuario({ cedula, nombre, correo, hashedPassword, rol });
+  const contrasenaSegura = await bcrypt.hash(contrasena, 10);
+  const usuario = new Usuario({ cedula, nombre, correo, contrasenaSegura, rol });
   console.log('Creando cuenta para usuario:', usuario);
   return UsuarioRepository.create(usuario);
 }
