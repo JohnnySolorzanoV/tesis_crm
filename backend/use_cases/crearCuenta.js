9decf361169525192c64488e9a069711345d1273
JohnnySV <150264587+JohnnySolorzanoV@users.noreply.github.com>
2026-04-28 17:40:40 +0000

    feat: agregar funcionalidades para la gestión de abogados y clientes, incluyendo creación, actualización y eliminación

--- backend/use_cases/crearCuenta.js
+++ backend/use_cases/crearCuenta.js
@@ -5,7 +5,7 @@ const { UsuarioRepository } = require('../repositories/UsuarioRepository');
 
 async function CrearCuenta({ cedula, nombre, correo, contrasena, rol }) {
   const contrasenaSegura = await bcrypt.hash(contrasena, 10);
-  const usuario = new Usuario({ cedula, nombre, correo, contrasenaSegura, rol });
+  const usuario = new Usuario({ identificacion: cedula, nombre, correo, contrasena: contrasenaSegura, rol });
   console.log('Creando cuenta para usuario:', usuario);
   return UsuarioRepository.create(usuario);
 }
